a10408bad568bda98b67a6e47ff155e637be812d
Smog <smog@iraklis-MacBook-Pro.local>
2021-05-08 14:24:21 +0400

    tasks done

---
+++ src/app.js
@@ -0,0 +1,176 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Op } = require("sequelize");
+const { sequelize } = require("./model");
+const { getProfile } = require("./middleware/getProfile");
+const app = express();
+app.use(bodyParser.json());
+app.set("sequelize", sequelize);
+app.set("models", sequelize.models);
+
+app.get("/contracts/:id", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const { id } = req.params;
+  const contract = await Contract.findOne({ where: { id } });
+  if (!contract) return res.status(404).end();
+  if (
+    contract.ContractorId !== req.profile.id &&
+    contract.ClientId !== req.profile.id
+  ) {
+    return res
+      .status(403)
+      .json({ message: "dont have a permission to access this contract" });
+  }
+  res.json(contract);
+});
+
+app.get("/contracts", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const { id: profileId } = req.profile;
+  const contracts = await Contract.findAll({
+    where: { [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }] },
+  });
+  res.json(contracts);
+});
+
+app.get("/jobs/unpaid", getProfile, async (req, res) => {
+  const { Contract, Job } = req.app.get("models");
+  const { id: profileId } = req.profile;
+  const jobs = await Job.findAll({
+    where: { paid: null },
+    include: [
+      {
+        model: Contract,
+        where: {
+          status: "in_progress",
+          [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
+        },
+      },
+    ],
+  });
+  res.json(jobs);
+});
+
+app.post("/jobs/:job_id/pay", getProfile, async (req, res) => {
+  const { Contract, Job } = req.app.get("models");
+  const { job_id: jobId } = req.params;
+  const job = await Job.findOne({ where: { id: jobId }, include: [Contract] });
+  const contractor = await job.Contract.getContractor();
+  const client = await job.Contract.getClient();
+  const amountToPay = job.price;
+  if (client.balance < amountToPay) {
+    return res
+      .status(400)
+      .json({ message: "you dont have enough money to pay for this job" });
+  }
+  const t = await sequelize.transaction();
+  try {
+    client.balance -= amountToPay;
+    contractor.balance += amountToPay;
+    await client.save();
+    await contractor.save();
+    await t.commit();
+  } catch (err) {
+    console.log(err);
+    t.rollback();
+  }
+  res.json({ client, contractor, job });
+});
+
+app.post("/balances/deposit", getProfile, async (req, res) => {
+  const { Contract, Job } = req.app.get("models");
+  const { id: profileId } = req.profile;
+  const { amountToDeposit } = req.body;
+  const jobs = await Job.findAll({
+    attributes: {
+      include: [[sequelize.fn("SUM", sequelize.col("price")), "sumprice"]],
+    },
+    where: { paid: null },
+    include: [
+      {
+        model: Contract,
+        where: {
+          status: "in_progress",
+          ClientId: profileId,
+        },
+      },
+    ],
+    group: [sequelize.col("Contract.ClientId")],
+  });
+  const record = jobs[0];
+  const totalAmountToPayForJobs = record.get("sumprice");
+  if (totalAmountToPayForJobs / 4 < amountToDeposit) {
+    return res
+      .status(400)
+      .json(
+        "too much to deposit on one time pls split it in several transactions"
+      );
+  }
+  const client = await record.Contract.getClient();
+  const t = await sequelize.transaction();
+  try {
+    client.balance += amountToDeposit;
+    await client.save();
+    await t.commit();
+  } catch (err) {
+    console.log(err);
+    t.rollback();
+  }
+  res.json(client);
+});
+
+app.get("/admin/best-profession", async (req, res) => {
+  const { start, end } = req.query;
+  const { Contract, Job, Profile } = req.app.get("models");
+  const jobs = await Job.findAll({
+    attributes: {
+      include: [[sequelize.fn("MAX", sequelize.col("price")), "maxprice"]],
+    },
+    where: {
+      createdAt: {
+        [Op.gte]: new Date(start),
+        [Op.lte]: new Date(end),
+      },
+      paid: true,
+    },
+    include: [{ model: Contract, include: ["Contractor"] }],
+    group: [sequelize.col("Contract.Contractor.profession")],
+    order: [[sequelize.col("maxprice"), "DESC"]],
+    limit: 1,
+  });
+  const record = jobs[0];
+  const maxprice = record.get("maxprice");
+  res.json({
+    profession: record.Contract.Contractor.profession,
+    amount: maxprice,
+  });
+});
+
+app.get("/admin/best-clients", async (req, res) => {
+  const { start, end, limit } = req.query;
+  const { Contract, Job } = req.app.get("models");
+  const jobs = await Job.findAll({
+    attributes: {
+      include: [[sequelize.fn("SUM", sequelize.col("price")), "sumpaid"]],
+    },
+    where: {
+      createdAt: {
+        [Op.gte]: new Date(start),
+        [Op.lte]: new Date(end),
+      },
+      paid: true,
+    },
+    include: [{ model: Contract, include: ["Client"] }],
+    group: [sequelize.col("Contract.ClientId")],
+    order: [[sequelize.col("sumpaid"), "DESC"]],
+    limit: Number(limit),
+  });
+  const clients = jobs.map((j) => ({
+    paid: j.get("sumpaid"),
+    id: j.id,
+    fullName: `${j.Contract.Client.firstName} ${j.Contract.Client.lastName}`,
+  }));
+  res.json(clients);
+});
+
+module.exports = app;
